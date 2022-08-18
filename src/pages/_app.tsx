ae462977109c39ff3f90d52413069ca234244989
ericserka <ericserka@hotmail.com>
2022-08-18 00:52:16 -0300

    feat: reorganizando arquivos (criando pasta src) e primeiras configuracoes do trpc

---
+++ src/pages/_app.tsx
@@ -0,0 +1,31 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next'
+import {loggerLink} from '@trpc/client/links/loggerLink'
+import {httpBatchLink} from '@trpc/client/links/httpBatchLink'
+import superjson from 'superjson'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default withTRPC({
+  config({ctx}){
+    const url = 'http://localhost:3000/api/trpc'
+    const links = [loggerLink(),httpBatchLink({maxBatchSize:10,url})]
+    return {queryClientConfig:{defaultOptions:{
+      queries:{
+        staleTime:60
+      }
+    }},links,headers(){
+      if(ctx?.req) {
+        return {
+          ...ctx.req.headers,
+          'x-ssr':'1'
+        }
+      }
+      return {}
+    },transformer:superjson}
+  },
+  ssr:false
+})(MyApp)
